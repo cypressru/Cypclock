733da76ef4bfec25829864ebdad41e240849b050
cypressru <140924725+cypressru@users.noreply.github.com>
2023-10-11 03:51:07 -0500

    Start of a memory editor
    Revamped, added error splash screen

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import messagebox
 import psutil
+import errno
 
 # Create the main window
 root = tk.Tk()
@@ -26,9 +28,35 @@ def select_process():
     pid = int(listbox.get(selection[0]).split()[0])
 
     # Open a handle to the selected process
-    process_handle = open(f"/proc/{pid}/mem", "r+b")
+    try:
+        process_handle = open(f"/proc/{pid}/mem", "r+b")
+    except IOError as e:
+        if e.errno == errno.EACCES:
+            tk.messagebox.showerror("Error", "Permission denied. Try running the script with elevated privileges.")
+        else:
+            tk.messagebox.showerror("Error", f"Error opening process handle: {e}")
+        return
 
-    # TODO: Use the process handle to read and write memory
+    # Create a text box to search the memory
+    search_box = tk.Text(root, height=1)
+    search_box.pack(padx=10, pady=10)
+
+    # Create a button to search the memory
+    def search_memory():
+        # Get the search string from the text box
+        search_string = search_box.get("1.0", tk.END).strip()
+
+        # Search the memory for the search string
+        process_handle.seek(0)
+        memory_dump = process_handle.read()
+        index = memory_dump.find(search_string.encode())
+        if index == -1:
+            tk.messagebox.showinfo("Search Results", "String not found")
+        else:
+            tk.messagebox.showinfo("Search Results", f"String found at offset {index}")
+
+    search_button = tk.Button(root, text="Search", command=search_memory)
+    search_button.pack(pady=10)
 
     # Close the handle to the process
     process_handle.close()
